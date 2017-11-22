9f0d3dd12df62df78580f22ee7cdf9c48efe7d99
Lucky River <18632674212@163.com>
2017-11-22 23:14:24 +0800

    H5 canvas 游戏

---
+++ js/Land.js
@@ -0,0 +1,21 @@
+var Land = Class.extend({
+	init : function(){
+		this.image = game.R["land"];
+		//x坐标
+		this.x = 0;
+	},
+	//更新
+	update : function(){
+		this.x -= 2;
+		if(this.x < -336){
+			this.x = 0 ;
+		}
+	},
+	//渲染
+	render : function(){
+		//336、112是图片的宽度、高度
+		game.ctx.drawImage(this.image , this.x , game.height - 112);
+		game.ctx.drawImage(this.image , this.x + 336 , game.height - 112);
+		game.ctx.drawImage(this.image , this.x + 336 * 2 , game.height - 112);
+	}
+});
